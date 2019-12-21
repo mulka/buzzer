6e6224f3775dbe9101ae4cab2b7dc4f7b3f4c118
Kyle Mulka <repalviglator@yahoo.com>
2019-12-21 15:44:07 -0500

    start implementation of auto buzz by time

--- utils.py
+++ utils.py
@@ -4,6 +4,10 @@ import unittest
 import boto3
 
 
+def get_auto_buzz_times():
+    return []
+
+
 def get_auto_buzz_config():
     dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table('apartment-buzzer-auto-buzz')
@@ -37,17 +41,25 @@ def _should_auto_buzz(config):
     return auto_buzz
 
 
+def _should_auto_buzz_times(times):
+    for time_entry in times:
+        if time_entry['start'] < time() < time_entry['end']:
+            return True
+    return False
+
+
 def should_auto_buzz():
     config = get_auto_buzz_config()
+    times = get_auto_buzz_times()
 
-    return _should_auto_buzz(config)
+    return _should_auto_buzz(config) or _should_auto_buzz_times(times)
 
 
 class AutoBuzzTestCase(unittest.TestCase):
     def test_auto_buzz_true(self):
         config = {
             'value': 'true',
-            'until': time() + 1
+            'until': time() + 60
         }
         auto_buzz = _should_auto_buzz(config)
         self.assertTrue(auto_buzz)
@@ -55,7 +67,23 @@ class AutoBuzzTestCase(unittest.TestCase):
     def test_auto_buzz_false(self):
         config = {
             'value': 'true',
-            'until': time() - 1
+            'until': time() - 60
         }
         auto_buzz = _should_auto_buzz(config)
+        self.assertFalse(auto_buzz)
+
+    def test_in_range(self):
+        times = [
+            {'start': time() - 60, 'end': time() + 60}
+        ]
+
+        auto_buzz = _should_auto_buzz_times(times)
+        self.assertTrue(auto_buzz)
+
+    def test_out_of_range(self):
+        times = [
+            {'start': time() - 60, 'end': time() - 1}
+        ]
+
+        auto_buzz = _should_auto_buzz_times(times)
         self.assertFalse(auto_buzz)
